0513672bad5ea78e8c9a0511250b937cce4851cc
DennisRono <dennisrkibet@gmail.com>
2022-07-10 16:50:01 +0300

    master where share replied dark

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const logger = require('morgan')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080
 const options = require('./config/corsOptions')
@@ -10,7 +11,9 @@ const authRoute = require('./routes/auth')
 
 //middlewares
 app.use(cors(options))
+app.user(logger('combined'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/auth', authRoute)
 
