490461dc99b99004e1f22dbcdc0f6a64aec2571b
DennisRono <dennisrkibet@gmail.com>
2022-07-10 16:48:03 +0300

    route seems flower sight per

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080
 const options = require('./config/corsOptions')
 
@@ -9,6 +10,7 @@ const authRoute = require('./routes/auth')
 
 //middlewares
 app.use(cors(options))
+app.use(bodyParser.json())
 
 app.use('/auth', authRoute)
 
