abad65312c3e0d2c47c81635b9c79adebe5d9cbf
DennisRono <dennisrkibet@gmail.com>
2022-07-10 16:57:01 +0300

    curious gather duty split inside

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+router.post('/register', (req, res, next) => {
+	try {
+		
+	} catch (err) {
+		console.err({error: err})
+	}
+})
